a5c8e1d05ae6d7194a77ca86b37a50130727a547
bxp06 <2769039274@qq.com>
2024-07-19 20:10:03 +0800

    Update 00-showcase.cpp

--- examples/00-showcase.cpp
+++ examples/00-showcase.cpp
@@ -1,25 +1,22 @@
 #include "openai.hpp"
-
 #include <iostream>
+#include <string>
 
 int main() {
-    openai::start("sk-xxx","",true,"https://agino.me/"); // Will use the api key provided by `OPENAI_API_KEY` environment variable
+    openai::start("sk-on-Jo5FAGExsug1vrAvXCQ","",true,"https://agino.me/"); // Will use the api key provided by `OPENAI_API_KEY` environment variable
     // openai::start("your_API_key", "optional_organization"); // Or you can handle it yourself
+
+    std::string userInput;
+    std::cout << "请输入您的问题： ";
+    std::getline(std::cin, userInput);
 
     auto completion = openai::chat().create(R"(
     {
         "model": "gpt-3.5-turbo",
-        "messages": [{"role": "user", "content": "What things can I do in Amsterdam?"}],
+        "messages": [{"role": "user", "content": userInput") }],
         "max_tokens": 4096,
         "temperature": 0
     }
     )"_json); // Using user-defined (raw) string literals
-    std::cout << "Response is:\n" << completion.dump(2) << '\n'; 
-
-    auto image = openai::image().create({
-        { "prompt", "A logo with a cello in a heart"},
-        { "n", 1 },
-        { "size", "512x512" }
-    }); // Using initializer lists
-    std::cout << "Image URL is: " << image["data"][0]["url"] << '\n'; 
+    std::cout << "AI的回答是：" << completion.dump(2) << '\n '; 
 }
